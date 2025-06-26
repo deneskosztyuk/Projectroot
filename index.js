f76f3a6b9391dbcb9e792b8b8263af8fa439285d
Denes <denes.kosztyuk@gmail.com>
2025-06-26 12:54:26 +0100

    Remove inline comment from gesture handler import in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 // This is the entry point for a React Native application.
 // It imports the necessary modules and registers the main application component.
-import 'react-native-gesture-handler'; // Import gesture handler for navigation support
+import 'react-native-gesture-handler'; 
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
