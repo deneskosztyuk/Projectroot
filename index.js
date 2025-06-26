318915ec9b84c010584331de3c79013d7df8fc56
Denes <denes.kosztyuk@gmail.com>
2025-06-26 12:54:10 +0100

    Add comments to clarify the entry point of the React Native application

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// This is the entry point for a React Native application.
+// It imports the necessary modules and registers the main application component.
+import 'react-native-gesture-handler'; // Import gesture handler for navigation support
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
